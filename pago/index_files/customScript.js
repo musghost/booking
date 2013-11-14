09f128b24ee5a6afbe6bcda80c29863266b367e4
Andres Cidel <ac@manoderecha.mx>
2013-11-13 18:34:00 -0600

    New changes

---
+++ pago/index_files/customScript.js
@@ -0,0 +1,7 @@
+//Custom javascript for booking flow
+var tagDebug =1;
+var customScript = function() {
+ var deb1 = WhiteLabel.getIbeData();
+var deb2 = WhiteLabel.getIbeData().cabinClass;
+ tagDebug =2;
+};
